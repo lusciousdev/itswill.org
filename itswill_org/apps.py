b9c6a1d298cde8e287af430bd5b83439d149a5ec
lusciousdev <admin@luscious.dev>
2024-03-18 20:01:31 -0500

    initial setup

---
+++ itswill_org/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ItswillOrgConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'itswill_org'
---
+++ itswill_org/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'itswill_org'
+urlpatterns = [
+]
