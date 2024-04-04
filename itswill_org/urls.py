479c3653f68ba2e9beb424e818d32dad4b3c3738
lusciousdev <admin@luscious.dev>
2024-04-03 20:12:08 -0500

    fix pets url resolution

--- itswill_org/urls.py
+++ itswill_org/urls.py
@@ -9,7 +9,7 @@ from . import api
 app_name = 'itswill_org'
 urlpatterns = [
   path("", views.IndexView.as_view(), name="index"),
-  path("pets", views.PetsView.as_view(), name="pets"),
+  path("pets/", views.PetsView.as_view(), name="pets"),
   path("month/", views.MonthListView.as_view(), name="month_list"),
   path("month/<int:year>/<int:month>/", cache_page(60 * 60)(views.MonthView.as_view()), name="month"),
   
