5865d666fd209d2f46aa2d3d8e377e925ede7d61
lusciousdev <admin@luscious.dev>
2024-11-19 22:22:07 -0600

    fix vod channel link

--- itswill_org/urls.py
+++ itswill_org/urls.py
@@ -34,6 +34,6 @@ urlpatterns = [
   path("api/v1/pets/",      api.get_pets_message,    name = "api_pets_message"),
   path("api/v1/recentpet/", api.get_most_recent_pet, name = "api_recent_pet"),
   path("api/v1/petsleft/",  api.get_pets_left,       name = "api_pets_left"),
-  path("api/v1/randgarf/",   api.get_random_garfield, name = "api_random_garfield"),
+  path("api/v1/randgarf/",  api.get_random_garfield, name = "api_random_garfield"),
   path("api/v1/test/",      api.test_endpoint,       name = "api_test"),
 ]
